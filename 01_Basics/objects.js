d65e2218f3fad723f701722e5a98fd1ff13b3d3c
Irfan Saeed <132193755+IrfanSaeednarejo@users.noreply.github.com>
2024-02-13 14:24:57 +0000

    objects basics done

--- 01_Basics/objects.js
+++ 01_Basics/objects.js
@@ -3,7 +3,15 @@ const obj1={
    name: "Irfan Saeed",
    age: 18,
    location:"Karachi",
-   field: "Computer Science"
+   field: "Computer Science",
+   email: "irfan@google.com",
+   isLoggedIn: false
 }
 
-console.log(obj1)
+console.log(obj1["email"])
+console.log(obj1["isLoggedIn"])
+obj1.email="irfan@maang.com"
+obj1["isLoggedIn"]=true
+console.log(obj1["email"])
+console.log(obj1["isLoggedIn"])
+
