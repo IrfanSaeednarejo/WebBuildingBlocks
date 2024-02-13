34b0e544058383de38f551c64039b561ec70a624
Irfan Saeed <132193755+IrfanSaeednarejo@users.noreply.github.com>
2024-02-13 12:54:58 +0000

    basics of arrays updated

--- 01_Basics/arrays.js
+++ 01_Basics/arrays.js
@@ -2,18 +2,28 @@ const arrays1=[0,2,3,4,5,6]
 // console.log(arrays1[5])
 
 // JavaScript arrays are resizable and can contain a mix of different data types.
-
 const arrays2= new Array("irfan",4,6,78,"clear",'A')
 
 const fruits = [];
 fruits.push("banana", "apple", "peach",3);
-console.log(fruits.length); // 4 elements in length
-console.log(fruits); // 4 elements
+// console.log(fruits.length); // 4 elements in length
+// console.log(fruits); // 4 elements
 
-fruits.pop() // remove the top last element from array stack
-console.log(fruits.length); // 3 elements in length
-console.log(fruits); // 3 elements
+// fruits.pop() // remove the top last element from array stack
+// console.log(fruits.length); // 3 elements in length
+// console.log(fruits); // 3 elements
 
-fruits[5]="pinaple" // added at index 5 where 3,4 is empty after poping
-console.log(fruits.length); // 6 elements in length
-console.log(fruits); // 4 elements print
+// fruits[5]="pinaple" // added at index 5 where 3,4 is empty after poping
+// console.log(fruits.length); // 6 elements in length
+// console.log(fruits); // 4 elements print
+
+// console.log("A",fruits); 
+// const newfruits1= fruits.slice(1,3)
+// console.log(newfruits1)
+// console.log("B",fruits)
+// const newfruits2= fruits.splice(1,3)
+// console.log(newfruits2)
+// console.log("C",fruits)
+
+
+
