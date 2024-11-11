3432de4c07769908f30f247c6f09534c773cee1c
Irfan Saeed <132193755+IrfanSaeednarejo@users.noreply.github.com>
2024-11-11 21:00:23 +0000

    new commit

---
+++ 01_Basics_js/functionTypes.js
@@ -0,0 +1,26 @@
+// Number() function
+console.log(Number("123"));          
+console.log(Number("123abc"));       
+console.log(Number("123.45"));       
+console.log(Number(true));          
+console.log(Number(false));         
+console.log(Number(null));           
+console.log(Number(undefined));     
+console.log(Number(""));             
+
+// parseInt() function
+console.log(parseInt("123"));        
+console.log(parseInt("123abc"));     
+console.log(parseInt("abc123"));     
+console.log(parseInt("123.45"));     
+console.log(parseInt(""));           
+console.log(parseInt("10", 2));      
+
+// parseFloat() function
+console.log(parseFloat("123.45"));   
+console.log(parseFloat("123.45abc"));
+console.log(parseFloat("abc123.45"));
+console.log(parseFloat(""));         
+console.log(parseFloat("123"));      
+console.log(parseFloat("0.123"));   
+
